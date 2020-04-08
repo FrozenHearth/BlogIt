455f4ab9581f7d5085e771c06187837cdb357a7e
Vishwanath Sastry <FrozenHearth@users.noreply.github.com>
2020-04-08 16:46:53 +0530

    Revert "Fixes #14"

--- src/components/Blog/BlogDetails.vue
+++ src/components/Blog/BlogDetails.vue
@@ -1,17 +1,12 @@
 <template>
   <div>
-    <Header />
     This is the blog details page
   </div>
 </template>
 
 <script>
-import Header from '../common/Header';
 export default {
-  name: 'BlogDetails',
-  components: {
-    Header
-  }
+  name: 'BlogDetails'
 };
 </script>
 
