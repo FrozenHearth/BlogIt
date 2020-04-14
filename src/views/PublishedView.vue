7e56a71d5f80eec088260bf9d54fb70f48df5034
Vishu <brs@Vishus-MacBook-Air.local>
2020-04-14 11:33:47 +0530

    Created a rough layout for MyPublishedDetails component

---
+++ src/views/PublishedView.vue
@@ -0,0 +1,13 @@
+<template>
+  <ViewPublished />
+</template>
+
+<script>
+import ViewPublished from '../components/container/ViewPublished';
+export default {
+  name: 'Blog',
+  components: {
+    ViewPublished
+  }
+};
+</script>
