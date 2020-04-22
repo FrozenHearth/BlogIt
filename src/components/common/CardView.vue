3b7b847db86f2d0b828042fd8d5d15802a9d6001
Vishu <brs@Vishus-MacBook-Air.local>
2020-04-22 18:39:44 +0530

    Redesigned card view for blog & myPublished list pages

--- src/components/common/CardView.vue
+++ src/components/common/CardView.vue
@@ -1,23 +1,32 @@
 <template>
-  <v-container fluid class="card-container">
+  <v-container fluid class="list-card-container">
     <template v-if="activePath === '/blogs'">
-      <v-card v-for="blog in blogs" :key="blog.id" class="card">
+      <v-card
+        max-width="800"
+        v-for="blog in blogs"
+        :key="blog.id"
+        class="list-card"
+      >
+        <v-img
+          v-if="blog.picture_url"
+          class="blog-image"
+          :src="blog.picture_url"
+        >
+        </v-img>
+
         <v-list-item>
-          <v-list-item-avatar
-            color="primary"
-            min-width="36"
-            width="36"
-            height="36"
-          >
-            <span id="author-initials" class="white--text headline ">{{
-              blog.author_name.charAt(0).toUpperCase()
-            }}</span>
-          </v-list-item-avatar>
           <v-list-item-content>
-            <v-list-item-title class="headline">{{
-              blog.title
-            }}</v-list-item-title>
-            <v-list-item-subtitle
+            <v-tooltip left>
+              <template v-slot:activator="{ on }">
+                <router-link class="read-more-link" :to="`/blog/${blog.id}`">
+                  <v-list-item-title v-on="on" class="blog-title">{{
+                    blog.title.charAt(0).toUpperCase() + blog.title.slice(1)
+                  }}</v-list-item-title>
+                </router-link>
+              </template>
+              <span>Read More</span>
+            </v-tooltip>
+            <v-list-item-subtitle class="blog-subtitle"
               >by
               {{
                 blog.author_name.charAt(0).toUpperCase() +
@@ -27,43 +36,60 @@
           </v-list-item-content>
         </v-list-item>
 
-        <v-img
-          v-if="blog.picture_url"
-          class="blog-image"
-          :src="blog.picture_url"
-        ></v-img>
-
         <v-card-text>
-          {{ blog.details | truncate(50) }}
+          {{ blog.details | truncate(200) }}
         </v-card-text>
 
+        <div class="tags-list">
+          <v-chip
+            v-for="(blog, index) in blog.tag_details"
+            :key="index"
+            outlined
+            class="tags-container ma-2"
+          >
+            {{ blog.tag }}
+          </v-chip>
+        </div>
+
+        <v-divider class="list-card-divider"></v-divider>
+
         <v-card-actions>
-          <router-link class="read-more-link" :to="`/blog/${blog.id}`">
-            <v-btn class="read-more-btn" text color="deep-purple accent-4">
-              Read More
-            </v-btn>
-          </router-link>
+          <v-chip outlined class="posted-date-btn">
+            {{ moment(blog.pub_date).fromNow() }}
+          </v-chip>
         </v-card-actions>
       </v-card>
     </template>
 
     <template v-if="activePath === '/myPublished'">
-      <v-card v-for="blog in publishedBlogs" :key="blog.id" class="card">
+      <v-card
+        v-for="blog in publishedBlogs"
+        :key="blog.id"
+        max-width="800"
+        class="list-card"
+      >
+        <v-img
+          v-if="blog.picture_url"
+          class="blog-image"
+          :src="blog.picture_url"
+        ></v-img>
+
         <v-list-item>
-          <v-list-item-avatar
-            color="primary"
-            min-width="36"
-            width="36"
-            height="36"
-          >
-            <span id="author-initials" class="white--text headline ">{{
-              blog.author_name.charAt(0).toUpperCase()
-            }}</span>
-          </v-list-item-avatar>
           <v-list-item-content>
-            <v-list-item-title class="headline">{{
-              blog.title
-            }}</v-list-item-title>
+            <v-tooltip left>
+              <template v-slot:activator="{ on }">
+                <router-link
+                  class="read-more-link"
+                  :to="`/myPublished/${blog.id}`"
+                >
+                  <v-list-item-title v-on="on" class="blog-title">{{
+                    blog.title.charAt(0).toUpperCase() + blog.title.slice(1)
+                  }}</v-list-item-title>
+                </router-link>
+              </template>
+              <span>Read More</span>
+            </v-tooltip>
+
             <v-list-item-subtitle
               >by
               {{
@@ -74,22 +100,27 @@
           </v-list-item-content>
         </v-list-item>
 
-        <v-img
-          v-if="blog.picture_url"
-          class="blog-image"
-          :src="blog.picture_url"
-        ></v-img>
-
         <v-card-text>
-          {{ blog.details | truncate(50) }}
+          {{ blog.details | truncate(200) }}
         </v-card-text>
 
+        <div class="tags-list">
+          <v-chip
+            v-for="(blog, index) in blog.tag_details"
+            :key="index"
+            outlined
+            class="tags-container ma-2"
+          >
+            {{ blog.tag }}
+          </v-chip>
+        </div>
+
+        <v-divider class="list-card-divider"></v-divider>
+
         <v-card-actions>
-          <router-link class="read-more-link" :to="`/myPublished/${blog.id}`">
-            <v-btn class="read-more-btn" text color="deep-purple accent-4">
-              Read More
-            </v-btn>
-          </router-link>
+          <v-chip outlined class="posted-date-btn">
+            {{ moment(blog.pub_date).fromNow() }}
+          </v-chip>
         </v-card-actions>
       </v-card>
     </template>
@@ -97,12 +128,14 @@
 </template>
 
 <script>
+import moment from 'moment';
 export default {
   name: 'CardView',
   props: ['blogs', 'publishedBlogs'],
   data() {
     return {
-      activePath: ''
+      activePath: '',
+      postedOn: Date
     };
   },
   filters: {
@@ -117,28 +150,65 @@ export default {
   },
   mounted() {
     this.activePath = this.$route.path;
+  },
+  methods: {
+    moment(date) {
+      return moment(date);
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.card-container {
-  display: flex;
-  flex-wrap: wrap;
+.list-card-container {
+  // display: flex;
+  // flex-wrap: wrap;
   position: relative;
+  margin: 0 auto;
   top: 5em;
 }
-.card {
-  margin: 2em 5em 5em 5em;
-  max-width: 35em;
+.list-card {
+  margin: 2em auto 5em auto;
+  max-width: 45em;
   position: relative;
   left: 2em;
   opacity: 0.9;
 }
+.list-card .v-list-item__content {
+  padding-left: 4em !important;
+  padding-right: 4em !important;
+}
+
+.list-card .v-card__text {
+  padding-left: 3.8em !important;
+  padding-right: 3.8em !important;
+}
+
+.blog-title {
+  color: rgba(0, 0, 0, 0.84);
+  font-weight: 600;
+  font-size: 2.2em;
+  margin-top: 1em;
+  white-space: pre-wrap !important;
+}
+
+.blog-subtitle {
+  position: relative;
+  top: 0.5em;
+}
+
+.tags-container {
+  border-radius: 0 !important;
+  color: rgba(0, 0, 0, 0.54);
+}
+.tags-list {
+  display: flex;
+  margin: 0 0 0 4em;
+  flex-wrap: wrap;
+  text-transform: capitalize;
+}
 .blog-image {
   width: 100%;
-  height: 25em;
-  filter: brightness(80%);
 }
 .blog-short-desc {
   color: white;
@@ -147,6 +217,11 @@ export default {
   bottom: 4em;
   left: -1em;
   font-weight: 800;
+}
+.list-card-divider {
+  margin-left: 4.8em;
+  width: 68em;
+  margin-top: 1.6em;
 }
 .blog-redirect {
   color: black;
@@ -158,7 +233,20 @@ export default {
 .read-more-link {
   text-decoration: none !important;
 }
+.read-more-link:hover {
+  text-decoration: underline !important;
+  text-decoration-color: black !important;
+}
 .read-more-btn {
-  margin: 0 auto;
+  margin: 0.5em 0 0.5em 2.8em;
+  border-radius: 0;
+  cursor: pointer;
+}
+.posted-date-btn {
+  margin: 0.5em 0 0.5em 2.8em;
+  border-radius: 0;
+  text-transform: capitalize;
+  color: rgba(0, 0, 0, 0.54);
+  font-size: 1.4em;
 }
 </style>
