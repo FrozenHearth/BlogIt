7657800d1b3943b3a3d9e1e8c381949cac919774
Vishu <brs@Vishus-MacBook-Air.local>
2020-04-27 15:49:52 +0530

    Split CardView component into multiple smaller components for better readability and reusability

---
+++ src/components/common/Cards/Card.vue
@@ -0,0 +1,141 @@
+<template>
+  <v-container fluid class="list-card-container">
+    <BlogCard v-if="activePath === '/blogs'" :blogs="blogs" />
+    <MyPublishedCard
+      :publishedBlogs="publishedBlogs"
+      v-if="activePath === '/myPublished'"
+    />
+    <MyDraftsCard v-if="activePath === '/myDrafts'" :myDrafts="myDrafts" />
+  </v-container>
+</template>
+
+<script>
+import BlogCard from './BlogCard';
+import MyPublishedCard from './MyPublishedCard';
+import MyDraftsCard from './MyDraftsCard';
+export default {
+  name: 'CardView',
+  props: ['blogs', 'publishedBlogs', 'myDrafts'],
+  components: {
+    BlogCard,
+    MyPublishedCard,
+    MyDraftsCard
+  },
+  data() {
+    return {
+      activePath: '',
+      postedOn: Date
+    };
+  },
+  mounted() {
+    this.activePath = this.$route.path;
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+// Using ::v-deep to apply styles to child components
+::v-deep .list-card-container {
+  margin: 0 auto;
+}
+::v-deep .list-card {
+  margin: 2em auto 2em auto;
+  position: relative;
+  left: 2em;
+  max-width: 65em;
+  display: flex;
+  opacity: 0.9;
+}
+::v-deep .list-card .v-list-item__content {
+  padding-left: 2em !important;
+}
+
+::v-deep .list-card .v-card__text {
+  padding-left: 2.4em !important;
+  padding-right: 3.8em !important;
+}
+
+::v-deep .blog-title {
+  color: rgba(0, 0, 0, 0.84);
+  font-weight: 600;
+  font-size: 2.2em;
+  line-height: 28px;
+  margin-top: 0.5em;
+  white-space: pre-wrap !important;
+}
+
+::v-deep .blog-details-text,
+.blog-subtitle {
+  margin: -1.5em 0 0 0.2em;
+}
+
+::v-deep .blog-details-text {
+  color: rgba(0, 0, 0, 0.6);
+}
+
+::v-deep .tags-container {
+  border-radius: 0 !important;
+  color: rgba(0, 0, 0, 0.54);
+}
+::v-deep .tags-list {
+  display: flex;
+  margin: -1em 0 1em 2.4em;
+  flex-wrap: wrap;
+  text-transform: capitalize;
+}
+::v-deep .main-content-container {
+  width: 50em;
+}
+::v-deep .blog-image-container {
+  width: 15em;
+}
+::v-deep .blog-image {
+  width: 100%;
+  object-fit: cover;
+  height: 100%;
+}
+::v-deep .card-actions-container {
+  margin-top: -2em;
+}
+::v-deep .list-card-divider {
+  margin-left: 4.8em;
+  width: 68em;
+  margin-top: 1.6em;
+}
+::v-deep .blog-redirect {
+  color: black;
+  font-size: 1.6em;
+  position: relative;
+  bottom: 4em;
+  left: -3em;
+}
+::v-deep .card-text-read-more-link,
+.read-more-link {
+  text-decoration: none !important;
+  position: relative;
+  right: 0.1em;
+}
+::v-deep .card-text-read-more-link {
+  color: #000 !important;
+}
+::v-deep .read-more-link:hover {
+  text-decoration: underline !important;
+  text-decoration-color: black !important;
+}
+::v-deep .read-more-btn {
+  margin: 0.5em 0 0.5em 2.8em;
+  border-radius: 0;
+  cursor: pointer;
+}
+::v-deep .clock-icon {
+  margin-right: 0.2em;
+}
+::v-deep .posted-date-btn {
+  margin: 0.5em 0 0.5em 0.8em;
+  border-radius: 0;
+  background: none !important;
+  line-height: 17px;
+  color: rgba(0, 0, 0, 0.54);
+  font-size: 1.4em;
+}
+</style>
---
+++ src/components/common/Cards/MyDraftsCard.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <v-card tile v-for="blog in myDrafts" :key="blog.id" class="list-card">
+      <div class="main-content-container">
+        <v-list-item>
+          <v-list-item-content>
+            <v-tooltip left>
+              <template v-slot:activator="{ on }">
+                <router-link
+                  class="read-more-link"
+                  :to="`/myDrafts/${blog.id}`"
+                >
+                  <v-list-item-title v-on="on" class="blog-title">{{
+                    blog.title.charAt(0).toUpperCase() + blog.title.slice(1)
+                  }}</v-list-item-title>
+                </router-link>
+              </template>
+              <span>Read More</span>
+            </v-tooltip>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-card-text class="blog-subtitle"
+          >by
+          {{
+            blog.author_name.charAt(0).toUpperCase() + blog.author_name.slice(1)
+          }}</v-card-text
+        >
+
+        <v-card-text class="blog-details-text">
+          {{ blog.details | truncate(50) }}
+          <router-link
+            class="card-textread-more-link"
+            :to="`/myDrafts/${blog.id}`"
+          >
+            <span>...read more </span>
+          </router-link>
+        </v-card-text>
+
+        <v-card-actions class="card-actions-container">
+          <v-chip class="posted-date-btn">
+            <v-icon class="clock-icon">mdi-clock-outline</v-icon>
+            {{ moment(blog.pub_date).fromNow() }}
+          </v-chip>
+        </v-card-actions>
+      </div>
+      <div class="blog-image-container">
+        <v-img
+          v-if="blog.picture_url"
+          class="blog-image"
+          :src="blog.picture_url"
+        ></v-img>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+export default {
+  name: 'MyDraftsCard',
+  props: ['myDrafts'],
+  methods: {
+    moment(date) {
+      return moment(date);
+    }
+  },
+  filters: {
+    truncate(value, size) {
+      if (!value) return '';
+      value = value.toString();
+      if (value.length <= size) {
+        return value;
+      }
+      return value.substring(0, size);
+    }
+  }
+};
+</script>
+
+<style></style>
